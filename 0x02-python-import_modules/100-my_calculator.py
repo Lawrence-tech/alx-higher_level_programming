f693e45ec6a32c2bbe17ed2a0fa02a14ffb2c704
lawrence-tech <lawrencesagini2@gmail.com>
2023-01-09 09:50:41 -0800

    Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    if argv[2] in {"+", "-", "*", "/"}:
+    if argv[2] in {"+", "-", "/", "*"}:
         a = int(argv[1])
         b = int(argv[3])
         if argv[2] == "+":
