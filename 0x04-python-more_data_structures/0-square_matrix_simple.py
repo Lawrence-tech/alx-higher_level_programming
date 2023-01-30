2f622edd86b0ef4473a2ff09e2ce8ca3fad2a99b
Lawrence-tech <lawrencesagini2@gmail.com>
2023-01-29 16:33:40 -0800

    executable simple square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [[x*x for x in row] for row in matrix]
+    new_matrix = []
+    for i in matrix:
+        tmp_list = list(map(lampda x: x * x, i))
+        new_matrix.append(tmp_list)
+    return (new_matrix)
