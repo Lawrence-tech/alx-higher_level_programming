385f62a317dc36e5d5cbdbf6dc234f29f19b9ab1
Lawrence-tech <lawrencesagini2@gmail.com>
2023-01-31 22:08:06 -0800

    2. Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -24,6 +24,6 @@ class Square:
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("size must be >=0")
+            raise ValueError("size must be >= 0")
         else:
             self._Square__size = size
