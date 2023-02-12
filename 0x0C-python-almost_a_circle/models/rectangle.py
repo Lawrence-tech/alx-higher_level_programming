0784497c0e4c583921fdd2ba109f178168f42437
Lawrence-tech <lawrencesagini2@gmail.com>
2023-02-12 15:46:03 -0800

    validate attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -29,3 +29,45 @@ class Rectangle(Base):
         if (value <= 0):
             raise ValueError("width must be > 0")
             self.Width = value
+
+    @property
+    def height(self):
+        """Retrieve the height of rectangle """
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """ set passet private attribute height """
+        if (type(value) is not int):
+            raise TypeError("height must be an integer")
+        if (value <= 0):
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """ Retrieve private instance attribute x """
+        return (self.__x)
+
+    @x.setter
+    def x(self, value):
+        """set private instance attribute x """
+        if (type(value) is not int):
+            raise TypeError("x must be an integeer")
+        if (value < 0):
+            raise ValueError("x must be  >= 0")
+            self.__x = value
+
+    @property
+    def y(self):
+        """Retrieve private instance attribute y """
+        return (self.__y)
+
+    @y.setter
+    def y(self, value):
+        """set private instance attribute y """
+        if (type(value) is not int):
+            raise TypeError("y must be an integer")
+        if (value < 0):
+            raise ValueError("y must be >= 0")
+            self.__y = value
