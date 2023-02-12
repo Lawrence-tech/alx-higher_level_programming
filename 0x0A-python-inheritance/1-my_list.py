6b030cb81038d1c3d8678f57f723d4412b9f8651
Lawrence-tech <lawrencesagini2@gmail.com>
2023-02-11 22:58:33 -0800

    fix my list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,11 +5,11 @@ Module that sorts a list int
 
 
 class Mylist(list):
-    """
-    Class that inherits from list
-    """
+#"""
+#   Class that inherits from list
+#   """
     def print_sorted(self):
-        """ prints the list, but sorted (ascending sort) """
+        """prints the list, but sorted (ascending sort)"""
         new_list = self[:]
         new_list.sort()
         print(new_list)
