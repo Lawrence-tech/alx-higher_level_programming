34b5094705050c693afd10ecea81e079266afdf6
Lawrence-tech <lawrencesagini2@gmail.com>
2023-02-12 03:18:20 -0800

    12. Pascals Triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""function that returns a list of lists with N sized pascal_triangle """
+"""function that returns a list of lists representing the pascal's triangle"""
 
 
 def pascal_triangle(n):
-    """Returns a pascal's tringle os size n """
+    """Returns a list of listsrepresenting the pascal's tringle of size n """
     triangle = []
     if n <= 0:
         return triangle
 
     for i in range(n)
-    row = [1] * (i + 1)
-    if i > 1:
-        for j in range(1, i):
-            row[j] = triangle[i-1][j-1] + triangle[i-1][j]
-    triangle.append(row)
+        row = [1] * (i + 1)
+        if i > 1:
+            for j in range(1, i):
+                row[j] = triangle[i-1][j-1] + triangle[i-1][j]
+        triangle.append(row)
 
 
-return (triangle)
+    return (triangle)
