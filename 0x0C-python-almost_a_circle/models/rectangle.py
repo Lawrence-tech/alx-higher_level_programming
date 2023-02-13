86dde574c5a6c36dd14bee8f1d56702488651e24
Lawrence-tech <lawrencesagini2@gmail.com>
2023-02-12 16:16:45 -0800

    __str__, dispaly, updates

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -85,3 +85,37 @@ class Rectangle(Base):
             for w in range(self.width):
                 print('#', end='')
             print('')
+
+    def __str__(self):
+        """overriding the __str__ method that returns a custom string """
+        mssg = "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}"\
+            .format(self.id, self.x, self.y, self.width, self.height)
+        return (mssg)
+
+    def update(self, *args, **kwargs):
+        """Method that assigns an argument to each attribute by Non-keyword
+            and key/value """
+        arlist = ["id", "width", "height", "x", "y"]
+        if (args and len(args) != 0):
+            for arl in range(len(args)):
+                if (arl == 0):
+                    super().__init(args[arl])
+                elif (arl < len(arlist)):
+                    setattr(self, arlist[arl], args[arl])
+        else:
+            for key, value in kwargs.items():
+                if (key == 'id'):
+                    super().__init__(value)
+                else:
+                    setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ returns the dictionary representation of a Rectangle """
+        my_dict = {
+            'id': self.id,
+            'width': self.width,
+            'height': self.height,
+            'x': self.x,
+            'y': self.y
+        }
+    return (my_dict)
