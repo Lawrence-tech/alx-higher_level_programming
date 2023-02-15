67bd9ffd3b93772f06e5a451f3bbc205479b32d6
Lawrence-tech <lawrencesagini2@gmail.com>
2023-02-15 00:17:29 -0800

    15. Delete by value

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,7 +2,7 @@
 def complex_delete(a_dictionary, value):
     if a_dictionary is None:
         return
-    for x, y in a_dictionary.copy().items():
+    for x, y in list(a_dictionary.items()):
         if y == value:
             del a_dictionary[x]
-        return a_dictionary
+    return a_dictionary
--- 0x04-python-more_data_structures/sq.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for i in matrix:
-        tmp_list = list(map(lampda x: x * x, i))
-        new_matrix.append(tmp_list)
-    return (new_matrix)
