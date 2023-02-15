60b1c79e960f02985546ca8acd1db85120a6cfa0
Lawrence-tech <lawrencesagini2@gmail.com>
2023-02-15 11:11:56 -0800

    10. ByteCode -> Python #5

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import math
+""" Circle Math"""
+
+
+class MagicClass:
+    """A circle class"""
+
+    def __init__(self, radius=0):
+        """Initialized circle"""
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """Return area of circle"""
+        return self.__radius ** 2 * math.pi
+
+    def circuference(self):
+        """Return circumference of circle"""
+        return 2 * math.pi * self.__radius
