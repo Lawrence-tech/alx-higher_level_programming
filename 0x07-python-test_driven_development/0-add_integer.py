a43ecf4471f77da6b73b1234aad6aba5f53d00a2
Lawrence-tech <lawrencesagini2@gmail.com>
2023-02-17 01:10:58 -0800

    0. Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,29 +1,11 @@
 #!/usr/bin/python3
-
-"""
-Integers ``addition`` module.
-"""
+"""Module contains function that adds two integer values"""
 
 
 def add_integer(a, b=98):
-    """Calculate the sum of ``a`` and ``b``.
-
-    Args:
-        a: First parameter
-        b: Second parameter
-    Raises:
-        TypeError: If a or b is neither integer nor float.
-    Notes:
-        Float parameters are cast into integers.
-    """
-    if type(a) is not int:
-        if type(a) is float:
-            a = int(a)
-        else:
-            raise TypeError("a must be an integer")
-    if type(b) is not int:
-        if type(b) is float:
-            b = int(b)
-        else:
-            raise TypeError("b must be an integer")
-    return a + b
+    """Adds two numbers as integers, second has defaut value of 98"""
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
