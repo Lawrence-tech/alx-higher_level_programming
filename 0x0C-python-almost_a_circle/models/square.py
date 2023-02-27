ad714eddd92d326cd44e853bf9719377a495a98d
Lawrence-tech <lawrencesagini2@gmail.com>
2023-02-27 00:11:39 -0800

    Square now

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,55 +1,81 @@
 #!/usr/bin/python3
-"""
-Program that defines a Square in base from Rectangle
-"""
+"""a class Square"""
+
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ class Square that inherits from Rectangle """
-    def __init(self, size, x=0, y=0, id=None):
-        """Conctructor """
-        super().__init__(size, size, x, y, id)
+    """class Square that inherits all attributes from class Rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """instantiate Square with attributes from Rectangle
+        Args:
+            size (int): The size of the new Square.
+            x (int): a variable, coordinate of the new Square.
+            y (int): a variable, coordinate of the new Square.
+            id (int): The identity of the new Square.
+        all of the args are contributed by Rectangle class
+        """
+        super().__init__(width=size, height=size, x=x, y=y, id=id)
 
     @property
     def size(self):
-        """ Retrieves the size of Square """
-        return (self.width)
+        """getter for size"""
+        return self.width or self.height
 
     @size.setter
     def size(self, value):
-        """set passet private attribute oof size """
+        """setter for size"""
         self.width = value
         self.height = value
 
-    def __str__(self):
-        """overriding the __str__ method that returns a custom string"""
-        mssg = "[Square] ({:d}) {:d}/{:d} - {:d}"\
-            .format(self.id, self.x, self.y, self.width)
-        return (mssg)
-
     def update(self, *args, **kwargs):
-        """method that assigns an argument to each attribute by Non-keyword and
-        key/value """
-        arlist = ["id", "size", "x", "y"]
-        if (args and len(args) != 0):
-            for arl in range(len(args)):
-                if (arl == 0):
-                    super().update(args[arl])
-                elif (arl < len(arlist)):
-                    setattr(self, arlist[arl], args[arl])
-        else:
+        """assigns attributes
+        *args:
+            - 1st argument should be the id attribute
+            - 2nd argument should be the size attribute
+            - 3rd argument should be the x attribute
+            - 4th argument should be the y attribute
+        """
+        if args and len(args) != 0:
+            n = 0
+            for arg in args:
+                if n == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif n == 1:
+                    self.size = arg
+                elif n == 2:
+                    self.x = arg
+                elif n == 3:
+                    self.y = arg
+                n += 1
+
+        elif kwargs and len(kwargs) != 0:
             for key, value in kwargs.items():
-                if (key == 'id'):
-                    super().update(value)
-                else:
-                    setattr(self, key, value)
+                if key == "id":
+                    if value is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = value
+                elif key == "size":
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
 
     def to_dictionary(self):
-        """returns the dictionary representation of a Square """
+        """returns dictionary representation of the Square"""
         return {
-            'id': self.id,
-            'size': self.size,
-            'x': self.x,
-            'y': self.y
-            }
+                "id": self.id,
+                "size": self.size,
+                "x": self.x,
+                "y": self.y,
+                }
+
+    def __str__(self):
+        """returns string representation of Square"""
+        return ("[Square] ({}) {}/{} - {}".
+                format(self.id, self.x, self.y, self.width or self.height))
