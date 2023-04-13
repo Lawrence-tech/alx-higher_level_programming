67a399a8494e06a0950f9b01bf3efb8b7788db00
Lawrence.tech <lawrencesagini2@gmail.com>
2023-04-12 19:39:21 -0700

    3. Rectangle #3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,12 +1,14 @@
+#!/usr/bin/node
+
 class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) {
+    if (w > 0 && h > 0) { // create empty object if w & h are <= 0
       this.width = w;
       this.height = h;
     }
   }
 
-  print () {
+  print () { // an instance method that prints the rectangle using the char X
     for (let h = this.height; h > 0; h--) {
       console.log('X'.repeat(this.width));
     }
