e1bb8e1c79a404e3b42feb3965a71738aec9b032
Lawrence.tech <lawrencesagini2@gmail.com>
2023-04-12 19:32:12 -0700

    3. Rectangle #3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,16 @@
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let h = this.height; h > 0; h--) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
