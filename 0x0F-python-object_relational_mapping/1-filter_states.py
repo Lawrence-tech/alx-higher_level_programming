74185c1fd0684276cf91524f7855d74ad34c26bb
Lawrence.tech <lawrencesagini2@gmail.com>
2023-04-22 16:38:28 -0700

    error fix

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,44 +3,30 @@
 Lists all states with a name starting with N (upper N)
 from the database hbtn_0e_0_usa.
 """
-
 import sys
 import MySQLdb
 
-
-def main(mysql_username: str, mysql_password: str, database_name: str) -> None:
-    """
-    Connects to the MySQL database and retrieves all states
-    with a name starting with 'N'. Prints the results to the console.
-    """
+if __name__ == '__main__':
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=mysql_username,
         passwd=mysql_password,
-        db=database_name,
-    )
+        db=database_name)
 
-    cursor = db.cursor()
+    cur = db.cursor()
 
-    cursor.execute(
-        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-    )
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' COLLATE
+                utf8mb4_general_ci ORDER BY id ASC")
 
-    query_rows = cursor.fetchall()
+    query_rows = cur.fetchall()
 
     for row in query_rows:
         print(row)
 
-    cursor.close()
+    cur.close()
     db.close()
-
-
-if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print("Usage: ./1-filter_states.py < mysql username >
-              < mysql password > < database name >")
-        sys.exit(1)
-
-    main(sys.argv[1], sys.argv[2], sys.argv[3])
