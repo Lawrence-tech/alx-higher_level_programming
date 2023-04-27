6abd50346e9b2b13c19e4d83b88fa37e4b8ec64c
Lawrence.tech <lawrencesagini2@gmail.com>
2023-04-27 15:06:48 -0700

    2. Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,2 +1,29 @@
 #!/usr/bin/python3
+"""
+Takes in an argument and displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument.
+"""
+import sys
+import MySQLdb
 
+if __name__ == '__main__':
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name)
+
+    search = sys.argv[4]
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(search))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
