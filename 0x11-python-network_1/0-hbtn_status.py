e2c6d8217e2937dd6e5a8022dee5ffa3101720fb
Lawrence.tech <lawrencesagini2@gmail.com>
2023-04-28 15:50:59 -0700

    0. What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
-
 """
 Python script that fetches https://alx-intranet.hbtn.io/status
 """
+# First Import the urllib.request module
 import urllib.request
 
+# Create a Request object with the URL
+req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
 
-def fetch():
-    """
-    The funtion to print a response of a specific url
-    """
-    url = "https://intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode('utf8')))
+# Open the URL and read the response
+with urllib.request.urlopen(req) as response:
+    # Get the content type of the response
+    content_type = response.getheader('Content-Type')
+    # Get the body of the response as bytes
+    body = response.read()
+    # Decode the body according to the content type
+    if content_type == 'text/html; charset=utf-8':
+        body = body.decode('utf-8')
+    elif content_type == 'application/json':
+        body = body.decode('json')
+    else:
+        # Use a default encoding if not specified
+        body = body.decode()
 
-
-if __name__ == "__main__":
-    fetch()
+# Display the body of the response
+print("Body response:")
+print("\t- type: {}".format(type(body)))
+print("\t- content: {}".format(body))
