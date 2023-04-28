5076ce700786dd10ea1b81064b0c77129040f40b
Lawrence.tech <lawrencesagini2@gmail.com>
2023-04-28 16:33:05 -0700

    0. What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -14,17 +14,9 @@ with urllib.request.urlopen(req) as response:
     content_type = response.getheader('Content-Type')
     # Get the body of the response as bytes
     body = response.read()
-    # Decode the body according to the content type
-    if content_type == 'text/html; charset=utf-8':
-        body = body.decode('utf-8')
-    elif content_type == 'application/json':
-        body = body.decode('json')
-    else:
-        # Use a default encoding if not specified
-        body = body.decode()
 
 # Display the body of the response
 print("Body response:")
 print("\t- type: {}".format(type(body)))
 print("\t- content: {}".format(body))
-print("\t- utf8 content: {}".format(body))
+print("\t- utf8 content: {}".format(body.decode('utf-8')))
