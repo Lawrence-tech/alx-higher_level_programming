586e6a2c90e9ad2a1d34684993826659662ea5c7
Lawrence.tech <lawrencesagini2@gmail.com>
2023-04-28 18:14:55 -0700

    5. Response header value #1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,14 +8,25 @@ displays the value of the variable X-Request-Id in the response header.
 import requests
 import sys
 
-# Get the URL from the command line argument
-url = sys.argv[1]
 
-# Send a GET request to the URL and get the response
-response = requests.get(url)
+# Define a main function that takes the URL as an argument
+def main(argv):
+    """
+    Method that takes in a URL, sends a request to the URL
+    and displays the value of the X-Request-Id variable
+    found in the header of the response
+    """
+    # Get the URL from the command line argument
+    url = argv[1]
+    # Send a GET request to the URL and get the response
+    response = requests.get(url)
+    # Get the value of the X-Request-Id header
+    x_request_id = response.headers.get('X-Request-Id')
+    # Print the value of the X-Request-Id header
+    print("{}".format(x_request_id))
 
-# Get the value of the X-Request-Id header
-header = response.headers.get('X-Request-Id')
 
-# Display the value of the X-Request-Id header
-print(header)
+# Check if the script is run as the main program
+if __name__ == "__main__":
+    # Call the main function with argv[1] as the argument
+    main(sys.argv)
