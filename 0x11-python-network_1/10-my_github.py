a982c3bc83a3a7ac555f2cb5adfdffc68cf0a276
Lawrence.tech <lawrencesagini2@gmail.com>
2023-05-08 08:25:02 -0700

    9. My GitHub!

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 """
-Python script that takes GitHub credentials (username and password)
-and uses the GitHub API to display id
+Python script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
 """
 
 from sys import argv
@@ -11,18 +12,22 @@ from requests.auth import HTTPBasicAuth
 
 def main(argv):
     """
-    This method takes your GitHub credentials (username and password) and
+    Script that takes your GitHub credentials (username and password) and
     uses the GitHub API to display your id.
     """
     user = argv[1]
     password = argv[2]
     response = requests.get('https://api.github.com/user',
-                            auth=HTTPBasicAuth(user, password))
+                            auth=HTTPBasicAuth('Lawrence-tech',
+                                               'github_pat_11AXVGSRA0aP2uIsQmGO'
+                                               'Wj_5YoMSYDkd0Z9779BSQGsIbjkHtUf'
+                                               'emtC9IRwiGF4EBw5HTC5RPD404QPXd'
+                                               'r'))
     try:
         profile_info = response.json()
         print(profile_info['id'])
-    except ValueError:
-        print('None')
+    except KeyError:
+        print('Error: Could not retrieve user ID')
 
 
 if __name__ == "__main__":
