9ad0d007b3a23b2053ff21143e78d68df098a2dd
Lawrence.tech <lawrencesagini2@gmail.com>
2023-05-19 01:46:55 -0700

    modify and fix error

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -11,4 +11,4 @@ ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode
 
 -- Field name in first_table
 ALTER TABLE first_table MODIFY `name` VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY `score` int(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY `score` int(11);
