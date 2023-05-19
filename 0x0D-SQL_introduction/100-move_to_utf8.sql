db85e7ff3fcf8985bf2e39a84794de71da60c789
Lawrence.tech <lawrencesagini2@gmail.com>
2023-05-19 01:39:41 -0700

    Do some fix

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -6,3 +6,4 @@ USE hbtn_0c_0;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- Field name in first_table
 ALTER TABLE first_table MODIFY `name` VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY `score` int CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
